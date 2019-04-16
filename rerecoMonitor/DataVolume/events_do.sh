4727325e52d658a0c9a02efd1982696839e4a28f
Shengquan <shengquan.tuo@cern.ch>
2019-04-16 11:31:33 -0500

    for total events

---
+++ rerecoMonitor/DataVolume/events_do.sh
@@ -0,0 +1,22 @@
+#echo "aod for each pd"
+#./getaodfromdas.sh
+#echo "get total aod"
+#./getaod.sh
+#echo "get aod at vandy"
+#./getaodvandy.sh
+
+current_date_time="`date +%m/%d/%Y-%H:%M`"
+echo -n $current_date_time"   "
+./events_getaodvandy.sh
+
+
+#echo "get aod taped"
+#./getaodtaped.sh
+#echo "raw for each pd"
+#./getrawfromdas.sh
+#echo "get total raw"
+#./getraw.sh
+#echo "get raw at fnal"
+#./getrawfnal.sh
+#echo "get raw taped"
+#./getrawtaped.sh
